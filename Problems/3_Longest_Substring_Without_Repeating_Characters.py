5611f9b8e72ab44fdd342776bf419607b67b58fd
LiaoU3 <vincent932693@gmail.com>
2021-06-03 00:40:20 +0800

    Better solution of problem 3

--- Problems/3_Longest_Substring_Without_Repeating_Characters.py
+++ Problems/3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,19 +1,39 @@
+######### Better Solution (with set)    ####################
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_length = 0
-        used_c = []
+        used_c = set()
         while max_length < len(s):
             for i in range(len(s)):
                 if s[i] not in used_c:
-                    used_c.append(s[i])
+                    used_c.add(s[i])
                 else:
                     if len(used_c) > max_length:
                         max_length = len(used_c)
-                    used_c = []
+                    used_c = set()
                     break
             start = s.index(s[i]) + 1
             s = s[start:]
-        return max_length if max_length >= len(used_c) else len(used_c)
+        return max(max_length ,len(used_c))
 
-# sol = Solution()
-# print(sol.lengthOfLongestSubstring("loddktdji"))
+
+sol = Solution()
+print(sol.lengthOfLongestSubstring("loddktdji"))
+
+#########   Bad Solution (with list)    #################
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         max_length = 0
+#         used_c = []
+#         while max_length < len(s):
+#             for i in range(len(s)):
+#                 if s[i] not in used_c:
+#                     used_c.append(s[i])
+#                 else:
+#                     if len(used_c) > max_length:
+#                         max_length = len(used_c)
+#                     used_c = []
+#                     break
+#             start = s.index(s[i]) + 1
+#             s = s[start:]
+#         return max(max_length ,len(used_c))

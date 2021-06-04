23dcdd0b65b3fc1e2f1efaded8e410f2d8404f1c
LiaoU3 <vincent932693@gmail.com>
2021-06-04 22:53:53 +0800

    upload a better solution of problem3

--- Problems/3_Longest_Substring_Without_Repeating_Characters.py
+++ Problems/3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,24 +1,39 @@
+#### Best Solition (using hashtable and slidingwindow)
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        sub_str = ''
+        max_len = 0
+        for w in s:
+            while w in sub_str:
+                sub_str = sub_str[1:]
+            else:
+                sub_str += w
+                max_len = max(max_len, len(sub_str))
+        
+        return max_len
+
+# sol = Solution()
+# print(sol.lengthOfLongestSubstring("loddktdji"))
+
+
 ######### Better Solution (with set)    ####################
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length = 0
-        used_c = set()
-        while max_length < len(s):
-            for i in range(len(s)):
-                if s[i] not in used_c:
-                    used_c.add(s[i])
-                else:
-                    if len(used_c) > max_length:
-                        max_length = len(used_c)
-                    used_c = set()
-                    break
-            start = s.index(s[i]) + 1
-            s = s[start:]
-        return max(max_length ,len(used_c))
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         max_length = 0
+#         used_c = set()
+#         while max_length < len(s):
+#             for i in range(len(s)):
+#                 if s[i] not in used_c:
+#                     used_c.add(s[i])
+#                 else:
+#                     if len(used_c) > max_length:
+#                         max_length = len(used_c)
+#                     used_c = set()
+#                     break
+#             start = s.index(s[i]) + 1
+#             s = s[start:]
+#         return max(max_length ,len(used_c))
 
-
-sol = Solution()
-print(sol.lengthOfLongestSubstring("loddktdji"))
 
 #########   Bad Solution (with list)    #################
 # class Solution:

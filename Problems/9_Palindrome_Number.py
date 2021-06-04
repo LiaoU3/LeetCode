ef90022d667bc597ce14980a5924398a50189359
LiaoU3 <vincent932693@gmail.com>
2021-06-05 00:04:27 +0800

    upload a better solutopn of problem 9

--- Problems/9_Palindrome_Number.py
+++ Problems/9_Palindrome_Number.py
@@ -1,8 +1,20 @@
+## Faster solution
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        if x[0] == '-':
+        if x < 0 or(x>0 and x%10 ==0):
             return False
         else:
-            if x[::-1] == x:
-                return True
+            rev = 0
+            while x > rev:
+                rev = rev*10 + x % 10
+                x = x//10
+            return rev == x or rev//10 == x
+## This solution is not fast enough !
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         x = str(x)
+#         if x[0] == '-':
+#             return False
+#         else:
+#             if x[::-1] == x:
+#                 return True

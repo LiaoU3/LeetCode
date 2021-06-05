734c60acb72604cf4680a8f4461089df7aac530f
LiaoU3 <vincent932693@gmail.com>
2021-06-05 14:32:19 +0800

    upload the solution of problem 13

--- Problems/13_Roman_to_Integer.py
+++ Problems/13_Roman_to_Integer.py
@@ -1,3 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         val_ls = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M':1000}
+        sum = 0
+        prev_val = float('inf')
+        for c in s:        
+            sum += val_ls[c]
+            if prev_val < val_ls[c]:
+                sum -= 2 * prev_val
+            prev_val = val_ls[c]
+        return sum
+
+# solution = Solution()
+# print(solution.romanToInt('MCMXCIV'))

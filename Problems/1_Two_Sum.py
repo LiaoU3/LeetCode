d3278c251c3f8041fe3d869704d2a5003b1d1d0e
LiaoU3 <vincent932693@gmail.com>
2021-06-10 00:03:52 +0800

    upload a better solution of problem 1

--- Problems/1_Two_Sum.py
+++ Problems/1_Two_Sum.py
@@ -1,10 +1,22 @@
+# Time Complexity O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
+        # key = number, value = index
+        hash_table = {}
         for i in range(len(nums)):
             to_find_num = target - nums[i]
-            if to_find_num in nums[:i] or to_find_num in nums[i+1:]:
-                ans.append(i)
-                ans.append(nums.index(to_find_num, i+1))
-                return ans
-                break
+            if to_find_num in hash_table.keys():
+                return[hash_table[to_find_num], i] 
+            else:
+                hash_table[nums[i]] = i
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         ans = []
+#         for i in range(len(nums)):
+#             to_find_num = target - nums[i]
+#             if to_find_num in nums[:i] or to_find_num in nums[i+1:]:
+#                 ans.append(i)
+#                 ans.append(nums.index(to_find_num, i+1))
+#                 return ans
+#                 break

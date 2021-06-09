99710cdbb11f8156666361f91ac4d98a463f8ddd
LiaoU3 <vincent932693@gmail.com>
2021-06-09 15:03:35 +0800

    update another solution of problem 20

--- Problems/20_Valid_Parentheses.py
+++ Problems/20_Valid_Parentheses.py
@@ -1,18 +1,35 @@
+## 2 solutions below have the similar speed
 class Solution:
     def isValid(self, s: str) -> bool:
         pair = {')' : '(',
                 '}' : '{',
                 ']' : '['}
-        stack = ''
+        stack = []
         for c in s:
-            stack += c
             if c in pair.keys():
-                if  len(stack) >= 2 and stack[-2] == pair[c]:
-                    stack = stack[:-2]
+                if len(stack)>= 1 and stack[-1] == pair[c]:
+                    stack = stack[:-1]
                 else:
                     return False
-        return True if len(stack)==0 else False
+            else:
+                stack += c
+        return stack == []
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         pair = {')' : '(',
+#                 '}' : '{',
+#                 ']' : '['}
+#         stack = ''
+#         for c in s:
+#             stack += c
+#             if c in pair.keys():
+#                 if  len(stack) >= 2 and stack[-2] == pair[c]:
+#                     stack = stack[:-2]
+#                 else:
+#                     return False
+#         return True if len(stack)==0 else False
 
 solution = Solution()
-s = ']'
+s = '[]]]]]]]]]]]]]]]]]'
 print(solution.isValid(s))

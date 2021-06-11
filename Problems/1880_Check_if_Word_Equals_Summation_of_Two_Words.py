e9b702f00fb6f08568dc9dc53624431d48da5167
LiaoU3 <vincent932693@gmail.com>
2021-06-11 22:17:00 +0800

    upload a fancy solution of problem 1880 XD

--- Problems/1880_Check_if_Word_Equals_Summation_of_Two_Words.py
+++ Problems/1880_Check_if_Word_Equals_Summation_of_Two_Words.py
@@ -1,20 +1,26 @@
+# a fancy way to do this LOL
 class Solution:
     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
-        num1 = ''
-        for c in firstWord:
-            num1 += str(ord(c) - 97)
-        num1 = int(num1)
+        numberize = lambda s : int(''.join(str(ord(c) - 97) for c in s))
+        return numberize(firstWord) + numberize(secondWord) == numberize(targetWord)
 
-        num2 = ''
-        for c in secondWord:
-            num2 += str(ord(c) - 97)
-        num2 = int(num2)
+# class Solution:
+#     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+#         num1 = ''
+#         for c in firstWord:
+#             num1 += str(ord(c) - 97)
+#         num1 = int(num1)
 
-        num3 = ''
-        for c in targetWord:
-            num3 += str(ord(c) - 97)
-        num3 = int(num3)
-        return num1 + num2 == num3
+#         num2 = ''
+#         for c in secondWord:
+#             num2 += str(ord(c) - 97)
+#         num2 = int(num2)
+
+#         num3 = ''
+#         for c in targetWord:
+#             num3 += str(ord(c) - 97)
+#         num3 = int(num3)
+#         return num1 + num2 == num3
 solution = Solution()
 firstWord = "acb"
 secondWord = "cba"

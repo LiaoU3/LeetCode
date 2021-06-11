b95684b29abc8bd91d6859e2cef54e08e177540d
LiaoU3 <vincent932693@gmail.com>
2021-06-11 22:10:19 +0800

    upload the solution of problem 1880

---
+++ Problems/1880_Check_if_Word_Equals_Summation_of_Two_Words.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        num1 = ''
+        for c in firstWord:
+            num1 += str(ord(c) - 97)
+        num1 = int(num1)
+
+        num2 = ''
+        for c in secondWord:
+            num2 += str(ord(c) - 97)
+        num2 = int(num2)
+
+        num3 = ''
+        for c in targetWord:
+            num3 += str(ord(c) - 97)
+        num3 = int(num3)
+        return num1 + num2 == num3
+solution = Solution()
+firstWord = "acb"
+secondWord = "cba"
+targetWord = "cdb"
+print(solution.isSumEqual(firstWord, secondWord, targetWord))

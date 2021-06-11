96ef67ce0c1fa591e466dff0d908d3b8ae24a372
LiaoU3 <vincent932693@gmail.com>
2021-06-11 21:57:00 +0800

    upload a better solution of problem 35

--- Problems/35_Search_Insert_Position.py
+++ Problems/35_Search_Insert_Position.py
@@ -1,28 +1,44 @@
+# a clear way to do this
 class Solution:
-    def searchInsert(self, nums, target) -> int:
-        start = 0
-        end   = len(nums)
-        index = (end + start)//2
-        flag = 0
-        while True:
-            prev_index = index
-            index = (end + start)//2
-            num = nums[index]
-            if prev_index == index :
-                flag += 1
-                if flag == 2:
-                    if target < num:
-                        return index
-                    else:
-                        return index + 1
-            if num == target:
-                return index
-            elif num < target:
-                start = index 
+    def searchInsert(self, nums, target):
+        l , r = 0, len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid]== target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
             else:
-                end = index 
+                r = mid-1
+        return l
+
+
+#### Too redundant
+# class Solution:
+#     def searchInsert(self, nums, target) -> int:
+#         start = 0
+#         end   = len(nums)
+#         index = (end + start)//2
+#         flag = 0
+#         while True:
+#             prev_index = index
+#             index = (end + start)//2
+#             num = nums[index]
+#             if prev_index == index :
+#                 flag += 1
+#                 if flag == 2:
+#                     if target < num:
+#                         return index
+#                     else:
+#                         return index + 1
+#             if num == target:
+#                 return index
+#             elif num < target:
+#                 start = index 
+#             else:
+#                 end = index 
 
 solution = Solution()
-nums = [1, 2, 4, 6, 7]
+nums = [1, 2, 4, 6, 7, 9, 10, 12, 15]
 target = 3
 print(solution.searchInsert(nums, target))

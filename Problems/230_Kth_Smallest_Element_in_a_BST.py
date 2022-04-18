e64330ec123b30f0f12aa64a35ac3dafb20f5728
LiaoU3 <vincent932693@gmail.com>
2022-04-19 00:09:47 +0800

    upload another solution of problem 230

--- Problems/230_Kth_Smallest_Element_in_a_BST.py
+++ Problems/230_Kth_Smallest_Element_in_a_BST.py
@@ -1,29 +1,42 @@
 # Definition for a binary tree node.
-from typing_extensions import Self
-
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 class Solution:
-    def __init__(self):
-        self.k = None
-        self.ans = None
-
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        self.k = k
-        self.traverse(root)
-        return self.ans
+        stack = []
+        curr = root
+        stack.append(curr)
+        
+        while stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
 
-    def traverse(self, node):
-        if not node or self.k<0:
-            return
-        self.traverse(node.left)
-        self.k -= 1
-        if self.k == 0:
-            self.ans = node.val
-            return 
-        self.traverse(node.right)
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
+            
+# class Solution:
+#     def __init__(self):
+#         self.k = None
+#         self.ans = None
 
+#     def kthSmallest(self, root: TreeNode, k: int) -> int:
+#         self.k = k
+#         self.traverse(root)
+#         return self.ans
+
+#     def traverse(self, node):
+#         if not node or self.k<0:
+#             return
+#         self.traverse(node.left)
+#         self.k -= 1
+#         if self.k == 0:
+#             self.ans = node.val
+#             return 
+#         self.traverse(node.right)

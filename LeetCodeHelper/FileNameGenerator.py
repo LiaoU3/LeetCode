927c0832c4ecc806a17e9612379a27d840861cbb
LiaoU3 <vincent932693@gmail.com>
2022-05-21 23:59:08 +0800

    updarw FileNameGenerator

--- LeetCodeHelper/FileNameGenerator.py
+++ LeetCodeHelper/FileNameGenerator.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import pathlib
+path = str(pathlib.Path(__file__).parent.parent) + '\\Problems\\'
+
 name = input("Please input the title name : ")
 programing_language = input("Enter the programing language (py(default), c, cpp etc.) : ")
 programing_language = programing_language if programing_language else 'py'
@@ -8,7 +11,5 @@ num = name_split_list[0]
 alpha = name_split_list[1].replace(' ', '_')
 filename = num + alpha + '.' + programing_language
 
-# you can change the path as you want 
-path = 'C:/Users/vincent_liao/Desktop/github/LeetCode/Problems/'
 myfile = Path(path + filename)
 myfile.touch(exist_ok=True)

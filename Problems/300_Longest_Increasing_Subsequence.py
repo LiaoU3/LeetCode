8c4dbc408b46eaa620a0c7f119acd72bc509ff3b
LiaoU3 <vincent932693@gmail.com>
2022-05-26 00:03:27 +0800

    upload problem 300

---
+++ Problems/300_Longest_Increasing_Subsequence.py
@@ -0,0 +1,45 @@
+from typing import List
+
+# Time Complexity = O(N*Log(N))
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        def binary_search_insert_index(d, n):
+            left = 0
+            right = len(d) - 1
+            while left <= right:
+                middle = (left+right)//2
+                if n > d[middle]:
+                    left = middle + 1
+                elif n < d[middle]:
+                    right = middle - 1
+                else:
+                    return middle
+            return left
+        
+        dp = []
+        for num in nums:
+            i = binary_search_insert_index(dp, num)
+            if i == len(dp):
+                dp.append(num)
+            else:
+                dp[i] = num
+        return len(dp)
+
+# Time Complexity = O(N**2)
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         dp = [1 for _ in range(len(nums))]
+#         for i in range(1, len(nums)):
+#             for j in range(i):
+#                 if nums[j]<nums[i]:
+#                     dp[i] = max(dp[j] + 1, dp[i])
+#         return max(dp)
+
+def main():
+    sol = Solution()
+    nums = [10,9,2,5,3,7,101,18]
+    print(sol.lengthOfLIS(nums))
+
+if __name__ == '__main__':
+    main()

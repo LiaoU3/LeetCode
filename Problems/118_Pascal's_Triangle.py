9c41a4b316cd8d9ec6008aee9957437aac4579c8
LiaoU3 <vincent932693@gmail.com>
2022-06-07 12:48:35 +0800

    add problem 412 and 118

---
+++ Problems/118_Pascal's_Triangle.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = [[1]]
+        for i in range(numRows-1):
+            curr = [1]
+            for j in range(len(ans[-1])):
+                if j+1<len(ans[-1]):
+                    curr.append(ans[-1][j] + ans[-1][j+1])
+            curr.append(1)
+            ans.append(curr)
+        return ans

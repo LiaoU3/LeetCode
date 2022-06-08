b44e9c2fff16bed05b9438d7aaa746e1e753747d
LiaoU3 <vincent932693@gmail.com>
2022-06-08 09:48:40 +0800

    add problem 1332

---
+++ Problems/1332_Remove_Palindromic_Subsequences.py
@@ -0,0 +1,11 @@
+# Important ! It saya "subsequence" instead of "subarray"
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return 2
+            left += 1
+            right -= 1
+        return 1

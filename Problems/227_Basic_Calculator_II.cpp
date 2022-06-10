6bf2ba70be4b73417b07a52661cc06112220c587
LiaoU3 <vincent932693@gmail.com>
2022-06-10 09:15:10 +0800

    Modify problem 227(Done)

--- Problems/227_Basic_Calculator_II.cpp
+++ Problems/227_Basic_Calculator_II.cpp
@@ -10,12 +10,11 @@ public:
         vector<int> stack;
         char operand = '+';
         int curr_num = 0;
-        for(int i; i<s.length(); i++){
+        for(int i=0; i<s.length(); i++){
             if(isdigit(s[i])){
-                curr_num = curr_num*10 + s[i] - '0';
-                cout << curr_num <<endl;
+                curr_num = curr_num*10 - '0'+ s[i];
             }
-            if (s[i]=='+' | s[i]=='-' | s[i]=='*' | s[i]=='/'| i==s.length()-1){
+            if (s[i]==' ' || i==s.length()-1){
                 if(operand=='+'){
                     stack.push_back(curr_num);
                 }else if(operand=='-'){
@@ -37,8 +36,40 @@ public:
     }
 };
 
+// class Solution {
+// public:
+//     int calculate(string s) {
+//         vector<int> stack;
+//         char operand = '+';
+//         int curr_num = 0;
+//         for(int i=0; i<s.length(); i++){
+//             if(isdigit(s[i])){
+//                 curr_num = curr_num*10 - '0'+ s[i];
+//             }
+//             if (s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/'|| i==s.length()-1){
+//                 if(operand=='+'){
+//                     stack.push_back(curr_num);
+//                 }else if(operand=='-'){
+//                     stack.push_back(-curr_num);
+//                 }else if(operand=='*'){
+//                     stack[stack.size()-1] *= curr_num;
+//                 }else{
+//                     stack[stack.size()-1] /= curr_num;
+//                 }
+//                 operand = s[i];
+//                 curr_num = 0;
+//             }   
+//         }
+//         int total = 0;
+//         for (auto num:stack){
+//             total += num;
+//         }
+//         return total;
+//     }
+// };
+
 int main(){
-    string s = "3+2*2";
+    string s = "2147483647";
     Solution sol;
     cout <<sol.calculate(s)<<endl;
 }

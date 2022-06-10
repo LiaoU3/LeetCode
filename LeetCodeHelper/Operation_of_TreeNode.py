e811763e496dccca04215a8a87026d9c039a761e
LiaoU3 <vincent932693@gmail.com>
2022-06-10 21:45:10 +0800

    Modify Operation of TreeNode(Under construction)

--- LeetCodeHelper/Operation_of_TreeNode.py
+++ LeetCodeHelper/Operation_of_TreeNode.py
@@ -4,3 +4,40 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+def List2TreeNode(ls = []):
+    pass
+
+# Create a list from a ListNode
+def TreeNode2List(root):
+    ret = [root.val]
+
+    curr = [root]
+    keep_going = True
+    while keep_going:
+        nxt = []
+        keep_going = False
+        for node in curr:
+            if node != None:
+                keep_going = True
+                left  = node.left
+                right = node.right
+                nxt.append(left)
+                nxt.append(right)
+            else:
+                nxt.append(None)
+                nxt.append(None)
+
+        if len(set(nxt))!=1:
+            for node in nxt:
+                if node == None:
+                    ret.append(node)
+                else:
+                    ret.append(node.val)
+        curr = nxt
+    for i in range(len(ret)-1, -1, -1):
+        if ret[i] == None:
+            ret.pop()
+        else:
+            break
+    return ret

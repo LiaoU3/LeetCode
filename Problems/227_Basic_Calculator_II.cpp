bcbda85afb85656583d1e7ddb3a92b187fe926cc
LiaoU3 <vincent932693@gmail.com>
2022-06-10 09:18:37 +0800

    Modify problem 227

--- Problems/227_Basic_Calculator_II.cpp
+++ Problems/227_Basic_Calculator_II.cpp
@@ -14,7 +14,7 @@ public:
             if(isdigit(s[i])){
                 curr_num = curr_num*10 - '0'+ s[i];
             }
-            if (s[i]==' ' || i==s.length()-1){
+            if (s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/'|| i==s.length()-1){
                 if(operand=='+'){
                     stack.push_back(curr_num);
                 }else if(operand=='-'){
@@ -35,39 +35,6 @@ public:
         return total;
     }
 };
-
-// class Solution {
-// public:
-//     int calculate(string s) {
-//         vector<int> stack;
-//         char operand = '+';
-//         int curr_num = 0;
-//         for(int i=0; i<s.length(); i++){
-//             if(isdigit(s[i])){
-//                 curr_num = curr_num*10 - '0'+ s[i];
-//             }
-//             if (s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/'|| i==s.length()-1){
-//                 if(operand=='+'){
-//                     stack.push_back(curr_num);
-//                 }else if(operand=='-'){
-//                     stack.push_back(-curr_num);
-//                 }else if(operand=='*'){
-//                     stack[stack.size()-1] *= curr_num;
-//                 }else{
-//                     stack[stack.size()-1] /= curr_num;
-//                 }
-//                 operand = s[i];
-//                 curr_num = 0;
-//             }   
-//         }
-//         int total = 0;
-//         for (auto num:stack){
-//             total += num;
-//         }
-//         return total;
-//     }
-// };
-
 int main(){
     string s = "2147483647";
     Solution sol;

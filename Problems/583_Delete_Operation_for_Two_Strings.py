ee928b1a6ffb0f823f1b8ac03e7359d053138cac
LiaoU3 <vincent932693@gmail.com>
2022-06-14 09:22:03 +0800

    Add Problem 583 in python and cpp

---
+++ Problems/583_Delete_Operation_for_Two_Strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        len1 = len(word1)
+        len2 = len(word2)
+        dp = [[0 for _ in range(len2+1)] for _ in range(len1+1)]
+        for i in range(1, len1+1):
+            for j in range(1, len2+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return len1 + len2 - dp[-1][-1]*2

ed5ba24dd7ddf8d7b73a1d215ff96e702711b476
LiaoU3 <vincent932693@gmail.com>
2022-06-14 23:05:00 +0800

    Modify Problem 1192 in cpp

--- Problems/1192_Critical_Connections_in_a_Network.cpp
+++ Problems/1192_Critical_Connections_in_a_Network.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -8,8 +9,12 @@ public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         vector<vector<int>> graph(n);
         for(auto connect: connections){
-            graph[connect[0]].push_back(connect[1]);
-            graph[connect[1]].push_back(connect[0]);
+            if (find(graph[connect[0]].begin(), graph[connect[0]].end(), connect[1]) == graph[connect[0]].end()) {
+                graph[connect[0]].push_back(connect[1]);
+            }
+            if (find(graph[connect[1]].begin(), graph[connect[1]].end(), connect[0]) == graph[connect[1]].end()) {
+                graph[connect[1]].push_back(connect[0]);
+            }
         }
         vector<int> min_step_list(n, -1);
         vector<vector<int>> res;
@@ -24,14 +29,14 @@ public:
             if (child == parent){
                 continue;
             }
-            if(child == -1){    
+            if(min_step_list[child] == -1){    
                 min_step_list[curr] = min(min_step_list[curr], dfs(child, curr, curr_step+1, min_step_list, graph, res));
             }else{
                 min_step_list[curr] = min(min_step_list[curr], min_step_list[child]);
             }
         }
 
-        if(min_step_list[curr] == curr_step+1){
+        if(min_step_list[curr] == curr_step+1 && curr!=0){
             vector<int> temp;
             temp.push_back(curr);
             temp.push_back(parent);

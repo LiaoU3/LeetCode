39dffe74dcbfb553abd4f9aee7a7cdda72c71b77
LiaoU3 <vincent932693@gmail.com>
2022-06-22 10:59:25 +0800

    Modify Problem 876 in python, make it simple

--- Problems/876_Middle_of_the_Linked_List.py
+++ Problems/876_Middle_of_the_Linked_List.py
@@ -6,12 +6,12 @@ class ListNode:
 from typing import Optional
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr_node = head
-        middle_node = head
-        cnt = -1
-        while curr_node:
-            curr_node  = curr_node.next
-            if cnt %2 == 0:
-                middle_node = middle_node.next
-            cnt += 1
-        return middle_node
+        slow = head
+        fast = head
+        count = 0
+        while fast:
+            if count %2:
+                slow = slow.next
+            fast = fast.next
+            count += 1
+        return slow

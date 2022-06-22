b1452b6ef992b30c831fd4a5af5f281230e4cd1b
LiaoU3 <vincent932693@gmail.com>
2022-06-22 11:40:03 +0800

    Modify Problem 19 in python, make it easy to understand

--- Problems/19_Remove_Nth_Node_From_End_of_List.py
+++ Problems/19_Remove_Nth_Node_From_End_of_List.py
@@ -8,19 +8,24 @@ from Operation_of_ListNode import *
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        curr = head
-        point = head
-        while curr:
-            curr = curr.next
-            if n!=-1:
-                n -= 1
-            else:
-                point = point.next
-        if n == -1:
-            point.next = point.next.next
-            return head
-        else:
-            return point.next
+        fast = head
+        slow = head
+        slow_pre = head
+        while fast:
+            if n<=0:
+                slow_pre = slow
+                slow = slow.next
+            fast = fast.next
+            n -= 1
+        slow_pre.next = slow.next
+        # Slow is about to move
+        if n==0:
+            return head.next
+        # Didn't even move
+        if slow==slow_pre:
+            return None
+        # regular case
+        return head
 
 def main():
     sol = Solution()

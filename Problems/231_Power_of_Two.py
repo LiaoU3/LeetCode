bd16f9bb6782335bcca943b10c187f74b267a89c
LiaoU3 <vincent932693@gmail.com>
2022-06-29 06:19:31 +0800

    Add Problem 231 in python

---
+++ Problems/231_Power_of_Two.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while n:
+            if n==1:
+                return True
+            if n%2==1:
+                return False
+            n //= 2
+        return False

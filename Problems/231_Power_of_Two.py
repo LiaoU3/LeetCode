4dcefd1e47f41a37ca07680f24fd623d687a9a41
LiaoU3 <vincent932693@gmail.com>
2022-06-29 10:02:14 +0800

    Modify Problem 231 in python add a better solution with bit manipulation

--- Problems/231_Power_of_Two.py
+++ Problems/231_Power_of_Two.py
@@ -7,3 +7,7 @@ class Solution:
                 return False
             n //= 2
         return False
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return True if n!=0 and n&(n-1)==0 else False

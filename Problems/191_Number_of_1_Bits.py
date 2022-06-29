f973eeb9db6fa28eec9b1b8717d75794b9338a16
LiaoU3 <vincent932693@gmail.com>
2022-06-29 10:08:25 +0800

    Modify Problem 191 Add another solution

--- Problems/191_Number_of_1_Bits.py
+++ Problems/191_Number_of_1_Bits.py
@@ -1,11 +1,20 @@
 # Operation in bits would make it run faster
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
         cnt = 0
-        while n>0:
-            n = n&n-1
+        while n:
+            n &= n-1
             cnt += 1
         return cnt
+
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         cnt = 0
+#         while n>0:
+#             n = n&n-1
+#             cnt += 1
+#         return cnt
 
 # class Solution:
 #     def hammingWeight(self, n: int) -> int:

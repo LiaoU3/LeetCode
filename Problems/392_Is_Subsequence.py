f87abfd683f02514c2f9e5649b47f359df997b67
LiaoU3 <vincent932693@gmail.com>
2022-07-01 10:14:47 +0800

    Modify Problem 392 in python Add a cleaner solution

--- Problems/392_Is_Subsequence.py
+++ Problems/392_Is_Subsequence.py
@@ -1,5 +1,17 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        point = 0
+        if len(s)==0:
+            return True
+        for i in range(len(t)):
+            if s[point] == t[i]:
+                point += 1
+                if point ==len(s):
+                    return True
+        return False
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
         pointS = 0
         for i in range(len(t)):
             if pointS==len(s):

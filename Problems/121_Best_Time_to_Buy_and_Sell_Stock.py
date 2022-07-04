b7681baead6d264191fd8a74d321013318b80c54
LiaoU3 <vincent932693@gmail.com>
2022-07-04 14:00:10 +0800

    Modify Problem 121 in python Add another smart and clean solution

--- Problems/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ Problems/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,12 +1,22 @@
 from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ret = 0
-        curr_min = float('Inf')
-        for p in prices:
-            ret = max(ret, p - curr_min)
-            curr_min = min(curr_min, p)
-        return ret
+        buy = float('inf')
+        profit = 0
+        for price in prices:
+            buy = min(buy, price)
+            profit = max(profit, price-buy)
+        return profit
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         ret = 0
+#         curr_min = float('Inf')
+#         for p in prices:
+#             ret = max(ret, p - curr_min)
+#             curr_min = min(curr_min, p)
+#         return ret
 
 # class Solution:
 #     def maxProfit(self, prices: List[int]) -> int:
@@ -16,3 +26,8 @@ class Solution:
 #             add = dp[i-1] + prices[i] - prices[i-1]
 #             dp[i] = add if add>0 else 0
 #         return max(dp)
+
+if __name__ == '__main__':
+    sol = Solution()
+    prices = [7,1,5,3,6,4]
+    print(sol.maxProfit(prices))

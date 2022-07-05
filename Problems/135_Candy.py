a1520c5647e62d8b206b40e0104cd0ac42b9443a
LiaoU3 <vincent932693@gmail.com>
2022-07-05 13:52:29 +0800

    Modify Problem 135 in python Add a time complexity:O(n) solution

--- Problems/135_Candy.py
+++ Problems/135_Candy.py
@@ -13,39 +13,30 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies = [1]*len(ratings)
         cnt = 0
+        pre = res = 1
         for i in range(1, len(ratings)):
-            if ratings[i]>ratings[i-1]:
+            if ratings[i]>=ratings[i-1]:
                 if cnt:
-                    candies[i] = 2
-                    candies[i-1] = 1
-                    for j in range(i-1, i-cnt-2, -1):
-                        if ratings[j]>ratings[j+1] and candies[j]<=candies[j+1]:
-                            candies[j] = candies[j+1]+1
-                        elif ratings[j]>ratings[j+1] and candies[j]<=candies[j+1]:
-                            candies[j] = 1
+                    res += cnt*(cnt+1)//2
+                    if cnt >= pre:
+                        res += cnt - pre + 1
                     cnt = 0
+                    pre = 1
+                if ratings[i]>ratings[i-1]:
+                    pre += 1  
                 else:
-                    candies[i] = candies[i-1]+1
-            elif ratings[i]==ratings[i-1]:
-                candies[i] = 1
+                    pre = 1
+                res += pre
             else:
                 cnt += 1
-
         if cnt:
-            candies[i] = 1
-            for j in range(i-1, i-cnt-1, -1):
-                if ratings[j]>ratings[j+1] and candies[j]<=candies[j+1]:
-                    candies[j] = candies[j+1]+1
-                elif ratings[j]>ratings[j+1] and candies[j]<=candies[j+1]:
-                    candies[j] = 1
-            cnt = 0
-        print(ratings)
-        print(candies)
-        return sum(candies)
-                
+            res += cnt*(cnt+1)//2
+            if cnt >= pre:
+                res += cnt - pre + 1
+        return res
+
 if __name__ == '__main__':
     sol = Solution()
-    ratings = [1,2,3,5,4,3,2,1,4,3,2,1,3,2,1,1,2,3,4]
+    ratings = [1,3,2,2,1]
     print(sol.candy(ratings))

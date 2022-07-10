28d12420670fc17f2a4a029ebddcb2d08dd74b7c
LiaoU3 <vincent932693@gmail.com>
2022-07-10 19:07:28 +0800

    Modify Problem 62 in python Add a cleaner solution

--- Problems/62_Unique_Paths.py
+++ Problems/62_Unique_Paths.py
@@ -1,5 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for i in range(n)]for j in range(m)]
+        for c in range(n):
+            dp[0][c] = 1
+        for r in range(m):
+            dp[r][0] = 1
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r][c-1]+dp[r-1][c]
+        return dp[m-1][n-1]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0 for _ in range(n)]for _ in range(m)]
         dp[0][0] = 1
         for i in range(m):

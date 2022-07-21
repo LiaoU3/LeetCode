73295cd6388ac35e014b76a029f657dbd83f1a35
LiaoU3 <vincent932693@gmail.com>
2022-07-21 13:09:24 +0800

    Add Problem 543 in cpp

---
+++ Problems/543_Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// class Solution {
+// public:
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int maxLen = 0;
+//         depth(root, maxLen);
+//         return maxLen;
+//     }
+
+//     int depth(TreeNode* root, int &maxLen){
+//         if(!root) return 0;
+//         int left = depth(root->left, maxLen);
+//         int right = depth(root->right, maxLen);
+//         maxLen = max(maxLen, left+right);
+//         return max(left, right)+1;
+//     }
+// };
+
+
+class Solution {
+public:
+	int maxLen = 0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        depth(root);
+        return maxLen;
+    }
+
+    int depth(TreeNode* root){
+        if(!root) return 0;
+        int left = depth(root->left);
+        int right = depth(root->right);
+        maxLen = max(maxLen, left+right);
+        return max(left, right)+1;
+    }
+};
+

8105ed158d7ee62c0a5b67c8639c536856a31ffb
LiaoU3 <vincent932693@gmail.com>
2022-07-26 14:17:43 +0800

    Add Problem 322 in python

--- Problems/322_Coin_Change.py
+++ Problems/322_Coin_Change.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        if not amount:
+            return 0
+        dp = [float('Inf')]*(amount+1)
+        dp[0] = 0
+        for i in range(1, amount+1):
+            for coin in coins:
+                if coin<=i:
+                    dp[i] = min(dp[i], dp[i-coin]+1)
+
+        return -1 if dp[-1]==float('Inf') else dp[-1]
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         dp = collections.defaultdict(float('Inf'))
         dp[0] = 0
         for num in range(1, amount+1):

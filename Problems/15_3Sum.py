10536f22a84e983b63e5ab9c4b037b9c2fd31c26
LiaoU3 <vincent932693@gmail.com>
2022-08-05 08:11:39 +0800

    Add another solution to Problem 15 in python

--- Problems/15_3Sum.py
+++ Problems/15_3Sum.py
@@ -1,5 +1,36 @@
 from typing import List
 
+# Two pointer Solution
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        m = len(nums)
+        res = []
+        for i in range(m-2):
+            # if the nums[i] is bigger than 0 then nums[r] and nums[l] will definitely bigger than 0
+            if nums[i] > 0:
+                break
+            l  = i + 1
+            r  = m - 1
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            while l < r:
+                sumUp = nums[i] + nums[l] + nums[r]
+                if sumUp == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    # actually we just have to update l to a different nums, so we don't have to change r
+                    # while l < r and nums[r] == nums[r-1]:
+                        # r -= 1
+                    l += 1
+                    r -= 1
+                elif sumUp > 0:
+                    r -= 1
+                else:
+                    l += 1
+        return res
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()

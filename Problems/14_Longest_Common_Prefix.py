4a512e755541b2daa5609b435d26f996b55bed8a
LiaoU3 <vincent932693@gmail.com>
2022-08-05 09:06:26 +0800

    Add another solution to Problem 14 in python

--- Problems/14_Longest_Common_Prefix.py
+++ Problems/14_Longest_Common_Prefix.py
@@ -1,3 +1,24 @@
+# cleanest solution
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = strs[0]
+        for i in range(len(res)):
+            for string in strs[1:]:
+                if i == len(string) or strs[0][i] != string[i]:
+                    return res[:i]
+        return res
+
+# cleaner solution
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for string in strs[1:]:
+                if i == len(string) or strs[0][i] != string[i]:
+                    return res
+            res += strs[0][i]
+        return res
+
 class Solution:
     def longestCommonPrefix(self, strs) -> str:
         if not strs:

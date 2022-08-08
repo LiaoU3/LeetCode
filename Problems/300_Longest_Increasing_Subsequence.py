6791c7d9a2126e76e9825909f1ebfad97cc04b02
LiaoU3 <vincent932693@gmail.com>
2022-08-08 10:21:47 +0800

    Add another solution to Problem 300 in python

--- Problems/300_Longest_Increasing_Subsequence.py
+++ Problems/300_Longest_Increasing_Subsequence.py
@@ -1,6 +1,6 @@
 from typing import List
 
-# Time Complexity = O(N*Log(N))
+# Time Complexity : O(N*Log(N))
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
@@ -26,15 +26,79 @@ class Solution:
                 dp[i] = num
         return len(dp)
 
-# Time Complexity = O(N**2)
+
+# Time Complexity : O(N*Log(N))
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        maxLen = 0
+        n = len(nums)
+        dp = [0] * n
+
+        def binarySearch(left, right, target):
+            if left > right:
+                return left
+            middle = (left + right) // 2
+            if dp[middle] == target:
+                return middle
+            elif dp[middle] > target:
+                return binarySearch(left, middle-1, target)
+            else:
+                return binarySearch(middle+1, right, target)
+
+        for i in range(n):
+            index = binarySearch(0, maxLen-1, nums[i])
+            if index == maxLen:
+                dp[maxLen] = nums[i]
+                maxLen += 1
+            else:
+                dp[index] = nums[i]
+        return maxLen
+
+
+# Time Complexity : O(N**2)
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        maxLen = 0
+        n = len(nums)
+        dp = [0] * n
+        for i in range(n):
+            for j in range(maxLen):
+                if nums[i] <= dp[j]:
+                    dp[j] = nums[i]
+                    break
+            else:
+                dp[maxLen] = nums[i]
+                maxLen += 1
+        print(dp)
+        return maxLen
+
 # class Solution:
 #     def lengthOfLIS(self, nums: List[int]) -> int:
-#         dp = [1 for _ in range(len(nums))]
-#         for i in range(1, len(nums)):
-#             for j in range(i):
-#                 if nums[j]<nums[i]:
-#                     dp[i] = max(dp[j] + 1, dp[i])
-#         return max(dp)
+#         maxLen = 1
+#         n = len(nums)
+#         dp = [0] * n
+#         dp[0] = nums[0]
+        
+#         for i in range(1, n):
+#             for j in range(maxLen):
+#                 if nums[i] <= dp[j]:
+#                     dp[j] = nums[i]
+#                     break
+#             else:
+#                 dp[maxLen] = nums[i]
+#                 maxLen += 1
+#         print(dp)
+#         return maxLen
+
+# Time Complexity : O(N**2)
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1 for _ in range(len(nums))]
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+        return max(dp)
 
 def main():
     sol = Solution()

327b2abfb4702d4aaa46908409781b76d11cb6b8
LiaoU3 <vincent932693@gmail.com>
2022-08-16 12:44:53 +0800

    Add another clean solution to Problem 198 in python and cpp

--- Problems/198_House_Robber.cpp
+++ Problems/198_House_Robber.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int m = nums.size();
+        vector<int> dp(m, 0);
+        if (m == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < m; ++i) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[m-1];
+    }
+};
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
         if(nums.size()==2) return max(nums[0], nums[1]);
         vector<int> dp(nums.size(), 0);
--- Problems/198_House_Robber.py
+++ Problems/198_House_Robber.py
@@ -1,5 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        m = len(nums)
+        if m == 1:
+            return nums[0]
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, m):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[-1]
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums)<=2:
             return max(nums)
         dp = [0]*len(nums)

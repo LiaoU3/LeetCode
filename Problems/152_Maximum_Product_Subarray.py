fb245524bfc6f5edc98a9dd5f9027f614a660d8a
LiaoU3 <vincent932693@gmail.com>
2022-08-20 00:38:35 +0800

    Add another solution to Problem 152 in python

--- Problems/152_Maximum_Product_Subarray.py
+++ Problems/152_Maximum_Product_Subarray.py
@@ -9,3 +9,19 @@ class Solution:
             localMin = min(localMin*nums[i], nums[i])
             maxProduct = max(maxProduct, localMax)
         return maxProduct
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        localMax = nums[0]
+        localMin = nums[0]
+        globalMax = nums[0]
+        print(localMin, localMax, globalMax)
+        for i in range(1, len(nums)):
+            num = nums[i]
+            if num < 0:
+                localMax, localMin = max(num, localMin * num), min(num, localMax * num)
+            else:
+                localMax, localMin = max(num, localMax * num), min(num, localMin * num)
+            globalMax = max(globalMax, localMax)
+            print(localMin, localMax, globalMax)
+        return globalMax

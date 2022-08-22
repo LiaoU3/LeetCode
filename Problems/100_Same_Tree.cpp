b43c38e14ecf9cdd9e8e93385ffc16fb87ac1c05
LiaoU3 <vincent932693@gmail.com>
2022-08-22 09:31:53 +0800

    Add a clean solution to Problem 100 in python and cpp

--- Problems/100_Same_Tree.cpp
+++ Problems/100_Same_Tree.cpp
@@ -15,6 +15,16 @@ public:
         if (p == nullptr && q == nullptr) return true;
         if (p == nullptr || q == nullptr) return false;
         if (p -> val != q -> val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p -> val != q -> val) return false;
         bool l = isSameTree(p->left, q->left);
         if (!l) return false;
         bool r = isSameTree(p->right, q->right);

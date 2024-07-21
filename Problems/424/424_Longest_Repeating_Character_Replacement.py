a23fc0f6773068fa3d6823cfd9f5ca09556dbfeb
LiaoU3 <vincent932693@gmail.com>
2024-07-21 15:48:03 +0800

    Add new solution to problem 424 and 543

---
+++ Problems/424/424_Longest_Repeating_Character_Replacement.py
@@ -0,0 +1,40 @@
+def characterReplacement(s: str, k: int) -> int:
+    table = [0 for _ in range(26)]
+    l = r = res = 0
+    while r < len(s):
+        char = s[r]
+        table[ord(char) - ord('A')] += 1
+        win_size = r - l + 1
+        max_table = max(table)
+        if win_size - max_table <= k:
+            # Valid window
+            res = max(res, win_size)
+        else:
+            # Invalid window
+            char = s[l]
+            table[ord(char) - ord('A')] -= 1
+            l += 1
+        r += 1
+    return res
+
+
+s = "ABAA"
+k = 0
+print(characterReplacement(s, k))
+
+
+
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         table = [0]*26
+#         currMaxCnt = 0
+#         maxLen = 0
+#         l = 0
+#         for r in range(len(s)):
+#             table[ord(s[r])-ord('A')] += 1
+#             currMaxCnt = max(currMaxCnt, table[ord(s[r])-ord('A')])
+#             while r-l+1-currMaxCnt>k:
+#                 table[ord(s[l])-ord('A')]-= 1
+#                 l += 1
+#             maxLen = max(maxLen, r-l+1)
+#         return maxLen                

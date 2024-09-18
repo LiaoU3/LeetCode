61ae0b1db346d34d9899cb7f70ed4a24c1726d19
LiaoU3 <vincent932693@gmail.com>
2024-09-18 23:42:55 +0800

    Add a new solution to problem 746

--- Problems/746/746_Min_Cost_Climbing_Stairs.py
+++ Problems/746/746_Min_Cost_Climbing_Stairs.py
@@ -1,11 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = [0 for _ in range(len(cost))]
-        dp[0] = cost[0]
-        dp[1] = cost[1]
         for i in range(2, len(cost)):
-            dp[i] = min(dp[i-2:i]) + cost[i]
-        return min(dp[-1], dp[-2])
+            cost[i] += min(cost[i-1], cost[i-2])
+        return min(cost[-1], cost[-2])
+
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         dp = [0 for _ in range(len(cost))]
+#         dp[0] = cost[0]
+#         dp[1] = cost[1]
+#         for i in range(2, len(cost)):
+#             dp[i] = min(dp[i-2:i]) + cost[i]
+#         return min(dp[-1], dp[-2])
 
 # without using additional space
 # class Solution:

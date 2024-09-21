0a80a3550da799826ab55394a2779e220eb9df41
LiaoU3 <vincent932693@gmail.com>
2024-09-21 20:27:04 +0800

    Add solutions to problem 332

---
+++ Problems/332/332.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:        
+        directions = defaultdict(list)
+        tickets.sort(reverse=True)
+        for start, end in tickets:
+            directions[start].append(end)
+        res = []
+        def dfs(curr):
+            while directions[curr]:
+                next_dest = directions[curr].pop()
+                dfs(next_dest)
+            res.append(curr)
+        dfs("JFK")
+        return res[::-1]
+
+# TLE Solution
+# class Solution:
+#     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+#         directions = collections.defaultdict(list)
+#         tickets.sort()
+#         for start, end in tickets:
+#             directions[start].append(end)
+#         res = ["JFK"]
+#         def dfs(curr):
+#             if len(res) == len(tickets) + 1:
+#                 return True
+#             if not directions[curr]:
+#                 return False
+#             for i, end in enumerate(directions[curr]):
+#                 res.append(end)
+#                 directions[curr].pop(i)
+#                 if dfs(end):
+#                     return True
+#                 res.pop()
+#                 directions[curr].insert(i, end)
+#             return False
+#         dfs("JFK")
+#         return res

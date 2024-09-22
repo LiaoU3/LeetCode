2c37aba56506fb7ffe1b6f87a82d6e7017032260
LiaoU3 <vincent932693@gmail.com>
2024-09-22 20:08:39 +0800

    Add a new solution to problem 97

--- Problems/97/97_Interleaving_String.py
+++ Problems/97/97_Interleaving_String.py
@@ -1,5 +1,26 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        m = len(s1)
+        n = len(s2)
+        if m + n != len(s3):
+            return False
+        dp = [[False for _ in range(m + 1)] for _ in range(n + 1)]
+        dp[0][0] = True
+        for i in range(n + 1):
+            for j in range(m + 1):
+                if i == 0 and j == 0:
+                    continue
+                c = s3[i + j - 1]
+                # look up
+                if i - 1 >= 0 and dp[i - 1][j] and c == s2[i - 1]:
+                    dp[i][j] = True
+                # look left
+                if j - 1 >= 0 and dp[i][j - 1] and c == s1[j - 1]:
+                    dp[i][j] = True
+        return dp[-1][-1]
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
         

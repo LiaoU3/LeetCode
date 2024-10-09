79ee903d4bb9936ab4c023f3a4aa91cbf870da1f
LiaoU3 <vincent932693@gmail.com>
2024-10-09 16:47:05 +0800

    Add a new solution to problem 208

--- Problems/208/208_Implement_Trie_(Prefix_Tree).py
+++ Problems/208/208_Implement_Trie_(Prefix_Tree).py
@@ -9,6 +9,38 @@ class Trie:
             if c not in curr:
                 curr[c] = {}
             curr = curr[c]
+        curr["!"] = {}
+
+    def search(self, word: str) -> bool:
+        curr = self.trie
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        if "!" not in curr:
+            return False
+        return True
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.trie
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return True
+
+
+class Trie:
+
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.trie
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
         curr["$"] = True
 
 
@@ -27,7 +59,6 @@ class Trie:
                 return False
             curr = curr[c]
         return True
-            
 
 
 # Your Trie object will be instantiated and called as such:

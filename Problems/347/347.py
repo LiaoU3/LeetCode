6ebfb921eb4f354dcc690ca15fdf3085fb908c2b
LiaoU3 <vincent932693@gmail.com>
2024-10-15 01:14:45 +0800

    Add a solution to problem 347

---
+++ Problems/347/347.py
@@ -0,0 +1,42 @@
+# O(n)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash_map = defaultdict(int)
+        for num in nums:
+            hash_map[num] += 1
+        
+        buckets = [[] for _ in range(len(nums) + 1)]
+        max_freq = 0
+        for num in hash_map:
+            freq = hash_map[num]
+            max_freq = max(max_freq, freq)
+            buckets[freq].append(num)
+
+        res = []
+        for i in range(max_freq, -1, -1):
+            if not buckets[i]:
+                continue
+            res.extend(buckets[i])
+            if len(res) == k:
+                break
+        return res
+
+#  O(n log(k))
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash_map = defaultdict(int)
+        for num in nums:
+            hash_map[num] += 1
+        hp = []
+        for num in hash_map:
+            freq = hash_map[num]
+            if len(hp) < k:
+                heappush(hp, (freq, num))
+            else:
+                if hp[0][0] < freq:
+                    heappop(hp)
+                    heappush(hp, (freq, num))
+        res = []
+        for _ in range(k):
+            res.append(heappop(hp)[1])
+        return res

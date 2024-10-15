92ea8daaefc938e2a2a3f20a46a9d285cb77f16e
liaou3 <vincent.liao@canonical.com>
2024-10-15 11:46:14 +0800

    Add a new solution to problem 122

--- Problems/122/122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ Problems/122/122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -1,4 +1,16 @@
 from typing import List
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        for i in range(1, len(prices)):
+            diff = prices[i] - prices[i-1]
+            if diff > 0:
+                res += diff
+        return res        
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         total = 0

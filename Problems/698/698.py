265a31d2e15b6f9dd3aae41d77d84f73228b7b15
LiaoU3 <vincent932693@gmail.com>
2024-10-16 23:31:05 +0800

    Update the solution to problem 698

--- Problems/698/698.py
+++ Problems/698/698.py
@@ -17,6 +17,9 @@ class Solution:
                     continue
                 if subsetSum + nums[j] > target:
                     continue
+                # If nums[j] == nums[j - 1] and nums[j - 1] is not used, then we know that this is also not going to be used
+                if j > 0 and not used[j - 1] and nums[j] == nums[j - 1]:
+                    continue
                 used[j] = True
                 if backtrack(j + 1, k, subsetSum + nums[j]):
                     return True

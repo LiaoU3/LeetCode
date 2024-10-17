9a4ed26457d706c19a43a66e374a950475ae4bac
LiaoU3 <vincent932693@gmail.com>
2024-10-17 21:48:27 +0800

    Add a new solution to problem 53

--- Problems/53/53_Maximum_Subarray.py
+++ Problems/53/53_Maximum_Subarray.py
@@ -2,12 +2,21 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = nums[0]
-        currSum = nums[0]
-        for num in nums[1:]:
-            currSum = max(currSum + num, num)
-            maxSum = max(maxSum, currSum)
-        return maxSum
+        max_sum = -float("Inf")
+        curr_sum = 0
+        for num in nums:
+            curr_sum = max(curr_sum + num, num)
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         maxSum = nums[0]
+#         currSum = nums[0]
+#         for num in nums[1:]:
+#             currSum = max(currSum + num, num)
+#             maxSum = max(maxSum, currSum)
+#         return maxSum
 
 # Memory Limit Exceeded
 # class Solution:

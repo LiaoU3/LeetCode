476a770998fc8779ff67cf381207c2b1ab53ed20
LiaoU3 <vincent932693@gmail.com>
2024-10-17 20:14:05 +0800

    Add a new solution to problem 48

--- Problems/48/48_Rotate_Image.py
+++ Problems/48/48_Rotate_Image.py
@@ -2,6 +2,24 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse
+        for r in range(n):
+            for c in range(n // 2):
+                # Python way
+                matrix[r][c], matrix[r][n - 1 - c] = matrix[r][n - 1 - c], matrix[r][c]
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         m = len(matrix)
         
         for i in range(m-1):

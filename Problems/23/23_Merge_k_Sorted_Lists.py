0a144ea0fb86b3f1d81c98d08279730c48a4c3e4
LiaoU3 <vincent932693@gmail.com>
2024-10-19 19:17:31 +0800

    Add a new solution to problem 23

--- Problems/23/23_Merge_k_Sorted_Lists.py
+++ Problems/23/23_Merge_k_Sorted_Lists.py
@@ -8,6 +8,41 @@ from Operation_of_ListNode import *
 from typing import Optional
 from typing import List
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def merge(left, right):
+            if left > right:
+                return None
+            if left == right:
+                return lists[left]
+            if left + 1 == right:
+                return merge2list(lists[left], lists[right])
+            
+            middle = left + (right-left)//2
+            l1 = merge(left, middle)
+            l2 = merge(middle + 1, right)
+            return merge2list(l1, l2)
+
+        def merge2list(node1, node2):
+            if node1 is None and node2 is None:
+                return None
+            val1 = node1.val if node1 else float("Inf")
+            val2 = node2.val if node2 else float("Inf")
+            if val1 < val2:
+                node = ListNode(val1)
+                node1 = node1.next
+            else:
+                node = ListNode(val2)
+                node2 = node2.next
+            node.next = merge2list(node1, node2)
+            return node
+        return merge(0, len(lists)-1)
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
@@ -42,6 +77,24 @@ class Solution:
             return result.next
 
         return merge(0, len(lists)-1)
+
+
+# Slow
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        def sort():
+            min_node = (ListNode(float("Inf")), -1)
+            for i, node in enumerate(lists):
+                if node and node.val < min_node[0].val:
+                    min_node = (node, i)
+            if min_node[1] == -1:
+                return None
+            lists[min_node[1]] = lists[min_node[1]].next
+            min_node[0].next = sort()
+            return min_node[0]
+        return sort()
+
 
 def main():
     lists = [[1,4,5],[1,3,4],[2,6]]

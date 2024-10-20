c244e78daf9fcb6d92e5383792efd6dd1d9db35e
LiaoU3 <vincent932693@gmail.com>
2024-10-20 15:01:35 +0800

    Move solutions for problem 190 to folder 190

--- Problems/190/190_Reverse_Bits.py
+++ Problems/190/190_Reverse_Bits.py
@@ -3,7 +3,7 @@ class Solution:
         res = 0
         for _ in range(32):
             res <<= 1
-            remain = n&1
+            remain = n & 1
             res |= remain
             n >>= 1
         return res

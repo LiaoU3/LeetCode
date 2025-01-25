5bd97898bcd660e339c095933bdd10bdde6fd725
LiaoU3 <vincent932693@gmail.com>
2025-01-25 15:04:10 +0800

    Update the solution for 73

--- Problems/73/73.py
+++ Problems/73/73.py
@@ -1,29 +1,21 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        used = set()
-        direction = ((0, 1), (1, 0), (0, -1), (-1, 0))
-        m = len(board)
-        n = len(board[0])
-        def dfs(r, c, i):
-            if i == len(word):
-                return True
-            if not (0 <= r < m and 0 <= c < n):
-                return False
-            if board[r][c] != word[i]:
-                return False
-            if (r, c) in used:
-                return False
-            used.add((r, c))
-            for dir_r, dir_c in direction:
-                next_r = r + dir_r
-                next_c = c + dir_c
-                if dfs(next_r, next_c, i + 1):
-                    return True
-            used.remove((r, c))
-            return False
-
-        for r in range(m):
-            for c in range(n):
-                if dfs(r, c, 0):
-                    return True
-        return False
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = set()
+        cols = set()
+        ROW = len(matrix)
+        COL = len(matrix[0])
+        for r in range(ROW):
+            for c in range(COL):
+                if matrix[r][c] == 0:
+                    rows.add(r)
+                    cols.add(c)
+        for r in rows:
+            for c in range(COL):
+                matrix[r][c] = 0
+        
+        for r in range(ROW):
+            for c in cols:
+                matrix[r][c] = 0

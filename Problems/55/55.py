8ac01a7ebd70daf566033e048c023f1eefaea779
LiaoU3 <vincent932693@gmail.com>
2025-01-26 17:56:00 +0800

    Add a new solution to pronlem 55

--- Problems/55/55.py
+++ Problems/55/55.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+# Instead of moving from the start, we move the goal back to the start
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums) - 1
@@ -9,23 +9,42 @@ class Solution:
                 goal = i
         return True if goal == 0 else False
 
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        visited = set()
+# Still not fast enough, but this is the improvement of the next solution
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+        
+#         def greedy(i) -> bool:
+#             if i >= len(nums) - 1:
+#                 return True
+#             if nums[i] == -1:
+#                 return False
+#             steps = nums[i]
+#             nums[i] = -1
+#             for step in range(steps, 0, -1):
+#                 if greedy(i + step):
+#                     return True
+#             return False
+        
+#         return greedy(0)
 
-        def dfs(i):
-            if i >= len(nums) - 1:
-                return True
-            if i in visited:
-                return False
-            visited.add(i)
-            if i > 0 and nums[i] < nums[i - 1]:  # If the previsous one did not make it and current steps are smaller than the previous one, there is no way that current one could make it.
-                return False 
-            for j in range(1, nums[i] + 1):
-                if dfs(i + j):
-                    return True
-            return False
-        return dfs(0)
+# Not Fast enough
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         visited = set()
+
+#         def dfs(i):
+#             if i >= len(nums) - 1:
+#                 return True
+#             if i in visited:
+#                 return False
+#             visited.add(i)
+#             if i > 0 and nums[i] < nums[i - 1]:  # If the previsous one did not make it and current steps are smaller than the previous one, there is no way that current one could make it.
+#                 return False 
+#             for j in range(1, nums[i] + 1):
+#                 if dfs(i + j):
+#                     return True
+#             return False
+#         return dfs(0)
 
 # TLE
 # class Solution:

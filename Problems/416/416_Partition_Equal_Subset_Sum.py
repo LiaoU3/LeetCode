65fde078e17b15f59c2dfdd5733feb54708efbbe
LiaoU3 <vincent932693@gmail.com>
2025-02-12 22:20:37 +0800

    Add a new solution to problem 416

--- Problems/416/416_Partition_Equal_Subset_Sum.py
+++ Problems/416/416_Partition_Equal_Subset_Sum.py
@@ -28,3 +28,58 @@ class Solution:
             for i in range(target, num-1, -1):
                 dp[i] |= dp[i-num]
         return dp[-1]
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2:
+            return False
+        target = total // 2
+        self.res = False
+        seen = set()  # memorize it
+        def backtrack(curr, total, i):
+            if self.res:
+                return
+            if i == len(nums):
+                return
+            if total == target:
+                self.res = True
+                return
+            if total > target:
+                return
+            if (total, i) in seen:
+                return
+            seen.add((total, i))
+            backtrack(curr, total, i + 1)
+            curr.append(i)
+            backtrack(curr, total + nums[i], i + 1)
+
+        backtrack([], 0, 0)
+        return self.res
+
+
+# TLE
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2:
+            return False
+        target = total // 2
+        self.res = False
+        def backtrack(curr, total, i):
+            if self.res:
+                return
+            if i == len(nums):
+                return
+            if total == target:
+                self.res = True
+                return
+            if total > target:
+                return
+            backtrack(curr, total, i + 1)
+            curr.append(i)
+            backtrack(curr, total + nums[i], i + 1)
+
+        backtrack([], 0, 0)
+        return self.res

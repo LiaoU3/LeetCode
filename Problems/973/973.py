5fec7536665d303f8ff4e6e1adeb1421f8cca7f7
liaou3 <vincent932693@gmail.com>
2025-02-17 18:06:05 +0800

    Add a new solution to problem 973

--- Problems/973/973.py
+++ Problems/973/973.py
@@ -8,3 +8,31 @@ class Solution:
         for _ in range(k):
             res.append(heapq.heappop(heap)[1])
         return res
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        def distance(i):
+            return points[i][0] ** 2 + points[i][1] ** 2
+
+        def partition(l, r):
+            pivot_index = r
+            pivot_distance = distance(pivot_index)
+            i = l
+            for j in range(l, r):
+                if distance(j) < pivot_distance:
+                    points[i], points[j] = points[j], points[i]
+                    i += 1
+            points[i], points[pivot_index] = points[pivot_index], points[i]
+            return i
+
+        pivot_index = len(points)
+        l = 0
+        r = len(points) - 1
+        while pivot_index != k:
+            pivot_index = partition(l, r)
+            if pivot_index > k:
+                r = pivot_index - 1
+            else:
+                l = pivot_index + 1
+        return points[:k]

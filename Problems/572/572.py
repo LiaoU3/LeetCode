89274a6affc04fbd01293c67d1d869201f702e2b
liaou3 <vincent932693@gmail.com>
2025-02-18 11:10:02 +0800

    Add a new solution to pronblem 572:

--- Problems/572/572.py
+++ Problems/572/572.py
@@ -4,6 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if subRoot is None:
+            return True
+        if root is None:
+            return False
+        if self.check(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    
+    def check(self, node1, node2):
+        if node1 is None and node2 is None:
+            return True
+        if node1 is None or node2 is None:
+            return False
+        if node1.val != node2.val:
+            return False
+        return self.check(node1.left, node2.left) and self.check(node1.right, node2.right)
+
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         self.res = False

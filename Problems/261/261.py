1ccedc3fb131218145ceda94984456a99adb9f21
liaou3 <vincent932693@gmail.com>
2025-02-20 13:10:50 +0800

    Add a new solution to problem 261

--- Problems/261/261.py
+++ Problems/261/261.py
@@ -1,3 +1,29 @@
+from collections import defaultdict
+
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        tree = defaultdict(list)
+        for s, t in edges:
+            tree[s].append(t)
+            tree[t].append(s)
+
+        seen = set()
+
+        def dfs(pre_node, node):
+            if node in seen:
+                return False
+            seen.add(node)
+            for next_node in tree[node]:
+                if next_node == pre_node:
+                    continue
+                if not dfs(node, next_node):
+                    return False
+            
+            return True
+
+        return dfs(-1, 0) and len(seen) == n
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         graph = defaultdict(list)

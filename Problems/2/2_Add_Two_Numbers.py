7c4b1cb852e5bef9d56ae84f62d360a92e5f6d0c
LiaoU3 <vincent932693@gmail.com>
2025-02-22 16:45:39 +0800

    Add a new solution to problem 2

--- Problems/2/2_Add_Two_Numbers.py
+++ Problems/2/2_Add_Two_Numbers.py
@@ -21,6 +21,27 @@ class Solution:
             curr.next = ListNode(1)
         return ans.next
 
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 is None and l2 is None:
+            return None
+        res = ListNode()
+        res.val += l1.val if l1 else 0
+        res.val += l2.val if l2 else 0
+        
+        if res.val > 9:
+            res.val %= 10
+            l1 = l1.next if l1 and l1.next else ListNode()
+            l1.val += 1
+        else:
+            l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+        res.next = self.addTwoNumbers(l1, l2)
+
+        return res
+
+
 # class Solution:
 #     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 #         num1 = 0

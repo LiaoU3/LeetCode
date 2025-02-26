4dd162c8e465d4284be25292c9df68ee296b68e7
LiaoU3 <vincent932693@gmail.com>
2025-02-26 21:56:50 +0800

    Add solutions to problem 239

---
+++ Problems/239/239.py
@@ -0,0 +1,53 @@
+from heapq import heappush, heappop
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+
+        # Keep it monotonic decreasing by value
+        q = deque()  # (value, index)
+
+        for i in range(len(nums)):
+            while q and q[-1][0] < nums[i]:
+                q.pop()
+            q.append((nums[i], i))
+
+            if q[0][1] <= i - k:
+                q.popleft()
+            
+            if i + 1 >= k:
+                res.append(q[0][0])
+        return res
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        hp = []
+        res = []
+
+        for i in range(len(nums)):
+            while hp and hp[0][1] <= i - k:
+                heappop(hp)
+            if not hp or nums[i] > -hp[0][0]:
+                heappush(hp, (-nums[i], i))
+            if i < k - 1:
+                continue
+            res.append(-hp[0][0])
+        return res
+
+# Not fast enough
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         hp = []
+#         res = []
+
+#         for i in range(len(nums)):
+#             heappush(hp, (-nums[i], i))
+#             if i < k - 1:
+#                 continue
+#             while hp[0][1] <= i - k:
+#                 heappop(hp)
+#             res.append(-hp[0][0])
+#         return res
+
+s = Solution()
+nums = [1,3,1,2,0,5]

be38c218dd8526b04126ebcbb257497e58721445
LiaoU3 <vincent932693@gmail.com>
2025-02-27 22:59:49 +0800

    Add a new solution to problem 567

--- Problems/567/567_Permutation_in_String.py
+++ Problems/567/567_Permutation_in_String.py
@@ -20,9 +20,35 @@ class Solution:
                 return True
         return False
 
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        table1 = [0] * 26
+        table2 = [0] * 26
+
+        def is_permutation():
+            for i in range(26):
+                if table1[i] != table2[i]:
+                    return False
+            return True
+
+        for i in range(len(s1)):
+            table1[ord(s1[i]) - ord('a')] += 1
+            table2[ord(s2[i]) - ord('a')] += 1
+
+        if is_permutation():
+            return True
+        for i in range(len(s1), len(s2)):
+            table2[ord(s2[i]) - ord('a')] += 1
+            table2[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            if is_permutation():
+                return True
+        return False
+
 if __name__ == '__main__':
     sol = Solution()
     s1 = "a"
-    s2 = "ab"
-    # s2 = "eidbaooo"
+    # s2 = "ab"
+    s2 = "eidbaooo"
     print(sol.checkInclusion(s1, s2))

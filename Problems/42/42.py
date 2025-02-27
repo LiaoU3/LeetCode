6a5fa5f00e32e98dba4df78fff5255c663fcac20
liaou3 <vincent932693@gmail.com>
2025-02-27 17:23:30 +0800

    Add solutions to problem 42

--- Problems/42/42.py
+++ Problems/42/42.py
@@ -1,19 +1,48 @@
+from typing import List
+
+
+# Prefix & Suffix Arrays
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left = [0] * len(height)  # Max height on the left side
-        right = [0] * len(height)  # Max height on the right side
-        curr_max = 0
+
+        left = [0] * len(height)
+        right = [0] * len(height)
+
+        left[0] = height[0]
+        for i in range(1, len(height)):
+            left[i] = max(left[i - 1], height[i])
+
+        right[-1] = height[-1]
+        for i in range(len(height) - 2, -1, -1):
+            right[i] = max(right[i + 1], height[i])
+
+        res = 0
         for i in range(len(height)):
-            h = height[i]
-            left[i] = curr_max
-            curr_max = max(curr_max, h)
-        curr_max = 0
-        for i in range(len(height) - 1, -1, -1):
-            right[i] = curr_max
-            curr_max = max(curr_max, height[i])
-        total = 0
-        for i in range(len(height)):
-            water = min(left[i], right[i]) - height[i]
-            if water > 0:
-                total += water
-        return total
+            res += min(left[i], right[i]) - height[i]
+
+        return res
+
+
+# Strictly decreasin
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Strictly decreasing
+        stack = []  # (height, index)
+        res = 0
+        for i, h in enumerate(height):
+            while stack and stack[-1][0] <= h:
+                left_h, left_i = stack.pop()
+                res += (i - left_i - 1) * (left_h - base)
+                base = left_h
+            else:
+                if stack:
+                    res += (i - stack[-1][1] - 1) * (h - base)
+                    base = h
+            stack.append((h, i))
+            base = h
+        return res
+
+
+height = [4, 2, 0, 3, 2, 5]
+s = Solution()
+print(s.trap(height))

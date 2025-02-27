a47285d7baa58dff01eab8cee9c378ae8504d85e
LiaoU3 <vincent932693@gmail.com>
2025-02-27 23:00:48 +0800

    Update the solution to problem 684

--- Problems/684/684.py
+++ Problems/684/684.py
@@ -1,37 +1,26 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        parents = [i for i in range(len(edges) + 1)]
-        ranks = [ 1 for _ in range(len(edges) + 1)]
         
-        # Find the parent and update it
-        def find(node):
-            if node != parents[node]:
-                node = find(parents[node])
-            return node
-        # def find(node):
-        #     p = parents[node]
-        #     children = []
-        #     while node != parents[node]:
-        #         node = parents[node]
-        #         children.append(node)
-        #     for child in children:
-        #         parents[child] = node
-        #     return node
+        parent = [i for i in range(len(edges) + 1)]
+        rank = [1 for _ in range(len(edges) + 1)]
 
-        # Union 2 graphs together
-        def union(n1, n2):
-            p1 = find(n1)
-            p2 = find(n2)
+        def find(node):
+            if node == parent[node]:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(node1, node2):
+            p1 = find(node1)
+            p2 = find(node2)
             if p1 == p2:
                 return False
-            if ranks[p1] > ranks[p2]:
-                parents[p2] = p1
-                ranks[p1] += ranks[p2]
-            else:
-                parents[p2] = p1
-                ranks[p2] += ranks[p1]
+            if rank[p2] > rank[p1]:
+                p1, p2 = p2, p1
+            parent[p2] = p1
+            rank[p1] += rank[p2]
             return True
-
-        for n1, n2 in edges:
-            if not union(n1, n2):
-                return [n1, n2]
+        
+        for node1, node2 in edges:
+            if not union(node1, node2):
+                return [node1, node2]

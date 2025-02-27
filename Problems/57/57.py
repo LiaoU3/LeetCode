e81ea305801b6db67018cd2e08917a106887c156
LiaoU3 <vincent932693@gmail.com>
2025-02-27 08:00:12 +0800

    Add a new solution to problem 57

--- Problems/57/57.py
+++ Problems/57/57.py
@@ -1,5 +1,29 @@
 from typing import List
 
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        def binary_search(l, r):
+            if l > r:
+                return l
+            m = (l + r) // 2
+            if intervals[m][0] == newInterval[0]:
+                return m
+            elif intervals[m][0] > newInterval[0]:
+                return binary_search(l, m - 1)
+            else:
+                return binary_search(m + 1, r)
+        
+        i = binary_search(0, len(intervals) - 1)
+        intervals.insert(i, newInterval)
+
+        res = []
+        for j in range(max(i - 1, 0), len(intervals)):
+            if not res or res[-1][1] < intervals[j][0]:
+                res.append(intervals[j])
+            else:
+                res[-1][1] = max(res[-1][1], intervals[j][1])
+        return res
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
@@ -29,8 +53,8 @@ class Solution:
                 res.append(newInterval)
         return res[1:]
 
-intervals = []
+intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
 
-newInterval = [2,3]
+newInterval = [4,8]
 s = Solution()
 print(s.insert(intervals, newInterval))

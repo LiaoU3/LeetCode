87801fb3ba687b0f669502cb5fdc6fc63b6cbe59
LiaoU3 <vincent932693@gmail.com>
2025-03-01 12:19:41 +0800

    Add a solution to problem 207

--- Problems/207/207.py
+++ Problems/207/207.py
@@ -1,3 +1,6 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
@@ -23,3 +26,38 @@ class Solution:
             if not dfs(i):
                 return False
         return True
+
+
+# Slow
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        taken = set([i for i in range(numCourses)])
+        course = defaultdict(list)  # key: course, val: [prerequisite1, prerequisite2, ...]
+        for c, p in prerequisites:
+            course[c].append(p)
+            if c in taken:
+                taken.remove(c)
+
+        seen = set()
+        def dfs(c):
+            if c in taken:
+                return True
+            if c in seen:
+                return False
+            seen.add(c)
+            for p in course[c]:
+                if not dfs(p):
+                    return False
+            taken.add(c)
+            return True
+
+        for i in range(numCourses):
+            seen = set()
+            dfs(i)
+        return len(taken) == numCourses
+
+
+s = Solution()
+numCourses = 3
+prerequisites = [[0,1],[0,2],[1,2]]
+print(s.canFinish(numCourses, prerequisites))

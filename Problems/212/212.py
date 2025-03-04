b5e33c04c18453d3e916ebc84cea667d8928a056
LiaoU3 <vincent932693@gmail.com>
2025-03-04 18:26:40 +0800

    Add a solution to problem 212

--- Problems/212/212.py
+++ Problems/212/212.py
@@ -39,8 +39,47 @@ class Solution:
                 dfs(r, c, trie, "")
         return list(res)
 
-sol = Solution()
-board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
 
-words = ["oath","pea","eat","rain"]
+# TLE O(M * N * W * 4^L)
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        ROW = len(board)
+        COL = len(board[0])
+        
+        seen = set()
+        def dfs(r, c, i, j):
+            if j == len(words[i]):
+                return True
+            if not(0 <= r < ROW and 0 <= c < COL):
+                return False
+            if board[r][c] != words[i][j]:
+                return False
+            if (r, c) in seen:
+                return False
+            seen.add((r, c))
+            for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                if dfs(r + dr, c + dc, i, j + 1):
+                    seen.remove((r, c))
+                    return True
+            seen.remove((r, c))
+            return False
+
+
+        res = []
+        found = set()
+        for r in range(ROW):
+            for c in range(COL):
+                for i in range(len(words)):
+                    if i in found:
+                        continue
+                    if dfs(r, c, i, 0):
+                        res.append(words[i])
+                        found.add(i)
+        return res
+
+sol = Solution()
+board = [["o","a","b","n"],["o","t","a","e"],["a","h","k","r"],["a","f","l","v"]]
+
+words = ["oa","oaa"]
 print(sol.findWords(board, words))

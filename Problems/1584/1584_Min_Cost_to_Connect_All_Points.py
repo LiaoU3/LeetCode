9515e7b8bd7398deaccf30104f4afe893a77fdfa
liaou3 <vincent932693@gmail.com>
2025-03-04 08:38:16 +0800

    Add solutions to problem 1584

--- Problems/1584/1584_Min_Cost_to_Connect_All_Points.py
+++ Problems/1584/1584_Min_Cost_to_Connect_All_Points.py
@@ -1,73 +1,69 @@
+# O(n ** 2 * log(n))
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        if len(points) == 1:
-            return 0
-        def distance(p1, p2):
-            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        res = 0
-        weights = [[] for _ in range(len(points))]
-        for i in range(len(points)):
-            for j in range(len(points)):
-                if i == j:
-                    continue
-                d = distance(points[i], points[j])
-                weights[i].append((d, j))
 
-        used = set()
-        used.add(0)
-        frontier = []
-        for weight in weights[0]:
-            heapq.heappush(frontier, weight)
-        print(frontier)
-        while len(used) < len(points):
-            distance, point = heapq.heappop(frontier)
-            if point in used:
+        hash_map = defaultdict(list)
+        for i in range(len(points)):
+            for j in range(i, len(points)):
+                d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                hash_map[i].append((d, j))
+                hash_map[j].append((d, i))
+
+        seen = set()
+        res = 0
+        hp = [(0, 0)]  # distance, point
+        while len(seen) < len(points):
+            d, p = heappop(hp)
+            if p in seen:
                 continue
-            res += distance
-            used.add(point)
-            for weight in weights[point]:
-                heapq.heappush(frontier, weight)
+            seen.add(p)
+            res += d
+            for d2, p2 in hash_map[p]:
+                if p2 in seen:
+                    continue
+                heappush(hp, (d2, p2))
+
+        return res
+
+# O(n ** 2 * log(n))
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        rank = [1] * len(points)
+        parent = [i for i in range(len(points))]
+
+        def find(node):
+            if node == parent[node]:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
+
+        def union(node1, node2):
+            p1 = find(node1)
+            p2 = find(node2)
+            if p1 == p2:
+                return False
+            if rank[p1] < rank[p2]:
+                p1, p2 = p2, p1
+            parent[p2] = p1
+            rank[p1] += rank[p2]
+            return True
+        
+        edges = []
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                heappush(edges, (d, i, j))
+        
+        res = 0
+        cnt = 0
+        while edges and cnt < len(points) - 1:
+            d, i, j = heappop(edges)
+            if union(i, j):
+                res += d
+                cnt += 1
         return res
 
 
-# class Solution:
-#     def minCostConnectPoints(self, points) -> int:
-#         kruskal = Kruskal(points)
-#         kruskal.create_graph()
-#         return kruskal.go()
-
-# class Kruskal():
-#     def __init__(self, points):
-#         self.points = points
-#         self.graph = []
-#         self.n = len(points)
-#         self.roots = [i for i in range(self.n)]
-
-#     def create_graph(self):
-#         for i in range(self.n):
-#             for j in range(i+1, self.n):
-#                 self.graph.append([i, j, self.manhattan_distance(self.points[i], self.points[j])])
-#         self.graph.sort(key = lambda point : point[2])
-        
-#     def manhattan_distance(self, point1, point2):
-#         return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-    
-#     def go(self):
-#         cnt = 0
-#         index = 0
-#         total_distance = 0
-#         while cnt < self.n-1:
-#             point1 = self.graph[index][0]
-#             point2 = self.graph[index][1]
-#             if self.roots[point1] != self.roots[point2]:
-#                 point2_temp = self.roots[point2]
-#                 for i in range(self.n):
-#                     if self.roots[i] == point2_temp:
-#                         self.roots[i] = self.roots[point1]
-#                 total_distance += self.graph[index][2]
-#                 cnt += 1
-#             index += 1
-#         return total_distance
 
 def main():
     points = [[0,0],[2,2],[3,10],[5,2],[7,0]]

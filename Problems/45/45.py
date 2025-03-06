060c5286b52a223402618e66a0e9eb3eabb55ef2
liaou3 <vincent932693@gmail.com>
2025-03-06 17:53:49 +0800

    Add solutions to problem 45

--- Problems/45/45.py
+++ Problems/45/45.py
@@ -1,17 +1,15 @@
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        res = 0
-        l = r = 0
-
-        while r < len(nums) - 1:
-            new_r = r
-            for i in range(l, r + 1):
-                new_r = max(new_r,  i + nums[i])
-            l = r + 1
-            r = new_r
-            res += 1
-        return res
+        jump = 0
+        curr_end = 0
+        max_end = 0
+        for i in range(len(nums) - 1):
+            max_end = max(max_end, i + nums[i])
+            if i == curr_end:
+                jump += 1
+                curr_end = max_end
+        return jump
 
 
 # Slow
@@ -30,3 +28,28 @@ class Solution:
             for j in range(1, nums[i] + 1):
                 q.append((i + j, step + 1))
         return -1
+
+# Slow O(N**2)
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        cache = {}
+        def backtrack(i):
+            if i >= len(nums) - 1:
+                cache[i] = 1
+                return 0
+            if i in cache:
+                return cache[i]
+            min_steps = float("Inf")
+            for j in range(i + nums[i], i, -1):
+                min_steps = min(min_steps, backtrack(j))
+            cache[i] = 1 + min_steps
+            return cache[i]
+
+        return backtrack(0)
+
+sol = Solution()
+nums = [2,3,1,1,4]
+print(sol.jump(nums))

6f79494969ed711f35768588d05203d81a52f935
LiaoU3 <vincent932693@gmail.com>
2025-03-06 21:24:39 +0800

    Add solutions to problem 678

--- Problems/678/678.py
+++ Problems/678/678.py
@@ -1,39 +1,95 @@
 # Greedy
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        min_n_max = [0, 0]  # The min and max number of ')' can we currently have
+        left_min = left_max = 0
         for c in s:
             if c == "(":
-                min_n_max[0] += 1
-                min_n_max[1] += 1
-            elif c == "*":
-                if min_n_max[0] > 0:
-                    min_n_max[0] -= 1
-                min_n_max[1] += 1
+                left_min += 1
+                left_max += 1
+            elif c == ")":
+                left_min = max(0, left_min - 1)
+                left_max -= 1
             else:
-                if min_n_max[0] > 0:
-                    min_n_max[0] -= 1
-                min_n_max[1] -= 1
-            if min_n_max[1] < 0:
+                left_min = max(0, left_min - 1)
+                left_max += 1
+            if left_max < 0:
                 return False
-        return min_n_max[0] == 0
+        return left_min == 0
 
-# Wrong answer
+# O(n)
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        cnt = 0
-        stack = []
-        for c in s:
-            if c == "*":
-                cnt += 1
-            elif c == ")":
-                if stack and stack[-1] == "(":
-                    stack.pop()
-                else:
-                    stack.append(c)
+
+        left = []
+        star = []
+
+        for i, c in enumerate(s):
+            if c == "(":
+                left.append(i)
+            elif c == "*":
+                star.append(i)
             else:
-                stack.append(c)
-        if cnt >= len(stack):
-            return True
-        else:
-            return False
+                if left:
+                    left.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+
+        while star and left and star[-1] > left[-1]:
+            star.pop()
+            left.pop()
+        return len(left) == 0
+
+
+# TLE O(3**N)
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        def backtrack(i, stack):
+            if i == len(s):
+                return len(stack) == 0
+            for j in range(i, len(s)):
+                if s[j] == "(":
+                    stack.append(s[j])
+                elif s[j] == ")":
+                    if stack and stack[-1] == "(":
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    # ""
+                    if backtrack(j + 1, stack.copy()):
+                        return True
+                    # (
+                    if backtrack(j + 1, stack.copy() + ["("]):
+                        return True
+                    # )
+                    if stack and stack[-1] == "(":
+                        stack_ = stack.copy()
+                        stack_.pop()
+                        if backtrack(j + 1, stack_):
+                            return True
+            return len(stack) == 0
+
+        return backtrack(0, [])
+
+# Wrong answer
+# class Solution:
+#     def checkValidString(self, s: str) -> bool:
+#         cnt = 0
+#         stack = []
+#         for c in s:
+#             if c == "*":
+#                 cnt += 1
+#             elif c == ")":
+#                 if stack and stack[-1] == "(":
+#                     stack.pop()
+#                 else:
+#                     stack.append(c)
+#             else:
+#                 stack.append(c)
+#         if cnt >= len(stack):
+#             return True
+#         else:
+#             return False

6d6d5515c5efe545ed65f80ebe63b8efad2c3c73
liaou3 <vincent932693@gmail.com>
2025-03-13 21:23:20 +0800

    Add a solution to problem 621

--- Problems/621/621_Task_Scheduler.py
+++ Problems/621/621_Task_Scheduler.py
@@ -1,12 +1,22 @@
+from typing import List
+from collections import Counter
+from heapq import heappush, heappop
+
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         table = [0]*26
         for c in tasks:
             table[ord(c) - ord('A')] += 1
-        
+
         maxCnt = max(table)
         maxCntTaskCnt = 0
         for cnt in table:
             if cnt == maxCnt:
                 maxCntTaskCnt += 1
         return max((n+1) * (maxCnt-1) + maxCntTaskCnt, len(tasks))
+
+
+sol = Solution()
+tasks = ["A","A","A","B","B","B", "C","C","C", "D", "D", "E"]
+print(sol.leastInterval(tasks, 2))

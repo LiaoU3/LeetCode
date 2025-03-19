d1d2cadcbe791eacffe5995bf5562a8335533fda
liaou3 <vincent932693@gmail.com>
2025-03-19 09:19:46 +0800

    Add a new solution to problem 286

--- Problems/286/286.py
+++ Problems/286/286.py
@@ -21,3 +21,26 @@ class Solution:
                         grid[nr][nc] = rnd + 1
                         q.append((nr, nc))
             rnd += 1
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROW = len(grid)
+        COL = len(grid[0])
+        q = deque()  # (row, col, distance)
+
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c] == 0:
+                    q.append((r, c, 0))
+        if len(q) == 0:
+            return
+
+        while q:
+            r, c, d = q.popleft()
+            for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                nr = r + dr
+                nc = c + dc
+                # If on the board and not been traversed
+                if 0 <= nr < ROW and 0 <= nc < COL and grid[nr][nc] == 2147483647:
+                    grid[nr][nc] = d + 1
+                    q.append((nr, nc, d + 1))

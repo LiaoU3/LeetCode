186b8baf0599aff8ee1827cab5f69840d332958a
liaou3 <vincent932693@gmail.com>
2025-03-20 09:20:49 +0800

    Add a new solution to problem 20

--- Problems/287/287.py
+++ Problems/287/287.py
@@ -31,6 +31,15 @@ class Solution:
             curr = tmp
         return res
 
+# O(N*log(N))
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
+
+
 sol = Solution()
 nums = [1,3,4,2,2]
 print(sol.findDuplicate(nums))

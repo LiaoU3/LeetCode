855818e70a4b39399d3b14e9c4a5adcb6eab3402
LiaoU3 <vincent932693@gmail.com>
2025-04-10 21:43:39 +0800

    Add a new solution to problem 66

--- Problems/66/66.py
+++ Problems/66/66.py
@@ -8,3 +8,20 @@ class Solution:
         if digits[0] == 0:
             digits = [1] + digits
         return digits
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = digits[::-1] + [0]
+        digits[0] += 1
+
+        for i in range(len(digits) - 1):
+            if digits[i] == 10:
+                digits[i + 1] += 1
+                digits[i] = 0
+            else:
+                break
+        
+        digits = digits[::-1]
+        if digits[0] == 0:
+            digits = digits[1:]
+        return digits

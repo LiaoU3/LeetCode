719b768bc2d9f7e4117976c6a5efb71ec745ab1a
liaou3 <vincent932693@gmail.com>
2025-04-11 09:18:59 +0800

    Add a new solution to problem 131

--- Problems/131/131.py
+++ Problems/131/131.py
@@ -1,7 +1,35 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+
+        cache = {}  # key:(l, r), val: bool
+
+        def is_pal(l, r):
+            if l >= r:
+                return True
+            if s[l] != s[r]:
+                cache[(l, r)] = False
+            else:
+                cache[(l, r)] = is_pal(l + 1, r - 1)
+            return cache[(l, r)]
         res = []
-        
+        curr = []
+        def backtrack(i):
+            if i == len(s):
+                res.append(curr.copy())
+            for j in range(i, len(s)):
+                if is_pal(i, j):
+                    curr.append(s[i: j + 1])
+                    backtrack(j + 1)
+                    curr.pop()
+
+        backtrack(0)
+        return res
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+
         def backtrack(i, curr):
             if i == len(s):
                 res.append(curr.copy())

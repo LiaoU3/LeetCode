3391202f38db7b545e647c70549483f369c632ba
liaou3 <vincent932693@gmail.com>
2025-04-11 09:19:40 +0800

    Add a new solution to problem 130

--- Problems/130/130.py
+++ Problems/130/130.py
@@ -8,6 +8,37 @@ class Solution:
         """
         ROW = len(board)
         COL = len(board[0])
+        q = deque()
+        for r in range(ROW):
+            for c in range(COL):
+                if (r == 0 or r == ROW - 1 or c == 0 or c == COL - 1) and board[r][c] == "O":
+                    q.append((r, c))
+
+        # Change "O" to "#" if connected to the edges
+        while q:
+            r, c = q.popleft()
+            board[r][c] = "#"
+            for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                nr = r + dr
+                nc = c + dc
+                if 0 <= nr < ROW and 0 <= nc < COL and board[nr][nc] == "O":
+                    q.append((nr, nc))
+
+        # Change "#" back to "O" and change "O" to "X"
+        for r in range(ROW):
+            for c in range(COL):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "#":
+                    board[r][c] = "O"
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROW = len(board)
+        COL = len(board[0])
         not_surrounded = set()
         visited = set()
         def dfs(r, c):

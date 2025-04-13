8cabf09e5f945f914f79ec2d5f90c86f0908dab7
LiaoU3 <vincent932693@gmail.com>
2025-04-13 20:28:43 +0800

    Add a new solution to problem 213

--- Problems/213/213.py
+++ Problems/213/213.py
@@ -1,5 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return max(nums)
+        nums1 = [0, 0, 0] + nums[:-1]
+        nums2 = [0, 0, 0] + nums[1:]
+
+        dp1 = [0] * (len(nums) + 2)
+        dp2 = [0] * (len(nums) + 2)
+
+        for i in range(3, len(nums1)):
+            dp1[i] = max(dp1[i - 2], dp1[i - 3]) + nums1[i]
+            dp2[i] = max(dp2[i - 2], dp2[i - 3]) + nums2[i]
+
+        return max(dp1[-2:] + dp2[-2:])
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
         dp1 = [0] * len(nums)

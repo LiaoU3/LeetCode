4efbce284591697703ce504fd1696880c2249efc
liaou3 <vincent932693@gmail.com>
2025-05-13 13:08:49 +0200

    Add another solution to problem 3335

--- Problems/3335/3335.py
+++ Problems/3335/3335.py
@@ -17,3 +17,21 @@ class Solution:
             counter = new_counter
 
         return sum(counter) % MOD
+
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        dp = [[0] * 26 for _ in range(t + 1)]
+        MOD = 10 ** 9 + 7
+        for c in s:
+            dp[0][ord(c) - ord("a")] += 1
+
+        for i in range(1, t + 1):
+            for j in range(1, 26):
+                dp[i][j] = dp[i - 1][j - 1]
+                dp[i][j] %= MOD
+            dp[i][0] += dp[i - 1][-1]
+            dp[i][0] %= MOD
+            dp[i][1] += dp[i - 1][-1]
+            dp[i][1] %= MOD
+
+        return sum(dp[-1]) % MOD

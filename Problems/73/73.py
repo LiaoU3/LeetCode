598221e6b0b776831e3e2763d5568eafc8393b29
liaou3 <vincent932693@gmail.com>
2025-05-21 08:57:09 +0800

    Add another solution to problem 73

--- Problems/73/73.py
+++ Problems/73/73.py
@@ -3,6 +3,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        ROW = len(matrix)
+        COL = len(matrix[0])
+        rows = set()
+        cols = set()
+        for r in range(ROW):
+            for c in range(COL):
+                if matrix[r][c] == 0:
+                    rows.add(r)
+                    cols.add(c)
+
+        for r in range(ROW):
+            for c in range(COL):
+                if r in rows or c in cols:
+                    matrix[r][c] = 0
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
         rows = set()
         cols = set()
         ROW = len(matrix)
@@ -15,7 +36,7 @@ class Solution:
         for r in rows:
             for c in range(COL):
                 matrix[r][c] = 0
-        
+
         for r in range(ROW):
             for c in cols:
                 matrix[r][c] = 0

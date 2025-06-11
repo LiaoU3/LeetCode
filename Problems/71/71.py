0fcd166bba450817ab3e7ba3591d75e4a72c631e
LiaoU3 <vincent932693@gmail.com>
2025-06-12 07:45:47 +0800

    Add a solution to problem 71

---
+++ Problems/71/71.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        folders = path.split("/")
+        stack = []
+        for folder in folders:
+            if not folder:
+                continue
+            if folder == ".":
+                continue
+            if folder == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(folder)
+        return "/" + "/".join(stack)
